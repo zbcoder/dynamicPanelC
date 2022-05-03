7141e7e42de391bd003f8971067937166101bdfd
zbcoder <zburlov0303@mail.ru>
2022-05-03 15:07:02 +0500

    demo ver

---
+++ tempPanelProject/CustomPanel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace tempPanelProject
+{
+    internal class CustomPanel : Panel
+    {
+        Panel panel;
+        Label productName;
+        Label productDescription;
+        Label productInfo;
+        PictureBox image;
+        Form currentForm;
+        TableLayoutPanel markupPanel;
+        static int exampleNumb = 0;
+        public CustomPanel(Form katForm)
+        {
+            currentForm = katForm;
+            //exampleNumb++;
+        }
+
+        public void Create(TableLayoutPanel pan, int panelValue)
+        {
+            int temp = 0;
+            for(int i=0; i<panelValue; i++)
+            {
+                panel = new Panel();
+                panel.Size = new Size(400, 200);
+                panel.Dock = DockStyle.Fill;
+                //panel.Location = new Point(10, 10+210*exampleNumb);
+                panel.BackColor = SystemColors.ControlDark;
+                pan.Controls.Add(panel, 1, 0 + exampleNumb++);
+                //pan.Controls.Add(panel);
+
+                markupPanel = new TableLayoutPanel
+                {
+                    Dock = DockStyle.Fill,
+                    RowCount = 2,
+                    BackColor = SystemColors.ControlLight,
+                    ColumnCount = 2,
+                    CellBorderStyle = TableLayoutPanelCellBorderStyle.InsetDouble
+                };
+                panel.Controls.Add(markupPanel);
+
+                productName = new Label();
+                productName.Size = new Size(60, 30);
+                //productName.Location = new Point(10, 40);
+                productName.BackColor = SystemColors.Control;
+                productName.Dock = DockStyle.Fill;
+                productName.Text = "ProductName";
+                markupPanel.Controls.Add(productName, 1, 0);
+
+                productDescription = new Label();
+                productDescription.Size = new Size(60, 30);
+                //productDescription.Location = new Point(10, 80);
+                productDescription.BackColor = SystemColors.Control;
+                productDescription.Dock = DockStyle.Fill;
+                productDescription.Text = "ProductDescription";
+                markupPanel.Controls.Add(productDescription, 1, 1);
+
+                image = new PictureBox();
+                image.Size = new Size(70, 70);
+                image.Dock = DockStyle.Fill;
+                image.Image = Image.FromFile(@"test.jpg");
+                image.SizeMode = PictureBoxSizeMode.StretchImage;
+                markupPanel.Controls.Add(image, 0, 0);
+
+                productInfo = new Label();
+                productInfo.Size = new Size(60, 30);
+                productInfo.Dock = DockStyle.Fill;
+                productInfo.Text = "ProductInfo";
+                productInfo.BackColor = SystemColors.Control;
+                markupPanel.Controls.Add(productInfo, 0, 1);
+
+            }
+        }
+
+        public bool Fill()
+        {
+            return true;
+        }
+    }
+}
---
+++ tempPanelProject/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace tempPanelProject
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            int vertScrollWidth = SystemInformation.VerticalScrollBarWidth;
+
+            tableLayoutPanel1.Padding = new Padding(0, 0, vertScrollWidth, 0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CustomPanel cs = new CustomPanel(this);
+            cs.Create(tableLayoutPanel1, Convert.ToInt32(textBox1.Text));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(System.Windows.Forms.SizeType.Absolute, 200));
+            i++;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.Refresh();
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                if (Convert.ToInt16(textBox1.Text) > 20)
+                {
+                    MessageBox.Show("Не умеем генерировать столько :(\n Хорошо и быстро работает +-20 записей");
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+                }
+                try
+                {
+                    int s = Convert.ToInt32(textBox1.Text);
+                }
+                catch (System.FormatException)
+                {
+                    MessageBox.Show("Вы ввели символ! Пожалуйста,введите цифрy");
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+                }
+            }
+        }
+    }
+}
